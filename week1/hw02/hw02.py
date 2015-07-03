1df6d2cd968bde936249d1c3c3ad0da9a7523ff9
chris.zh <chris.zh@outlook.com>
2015-07-03 19:58:26 +0800

    add Homework2

---
+++ week1/hw02/hw02.py
@@ -0,0 +1,205 @@
+#########################
+# Composition Questions #
+#########################
+
+better_summation = 1  # REPLACE None WITH 1 or 2
+
+better_summation_explanation = """
+YOUR EXPLANATION HERE
+1.The variable names of the version 2 function are just like shit.
+2.The iteration of version 1 function is easier to understand.
+"""
+
+
+better_is_positive = None  # REPLACE None WITH 1 or 2
+
+better_is_positive_explanation = """
+YOUR EXPLANATION HERE
+"""
+
+
+######################
+# Required Questions #
+######################
+
+def square(x):
+    return x * x
+
+def triple(x):
+    return 3 * x
+
+def identity(x):
+    return x
+
+def increment(x):
+    return x + 1
+
+
+def piecewise(f, g, b):
+    """Returns the piecewise function h where:
+
+    h(x) = f(x) if x < b,
+           g(x) otherwise
+
+    >>> def negate(x):
+    ...     return -x
+    >>> abs_value = piecewise(negate, identity, 0)
+    >>> abs_value(6)
+    6
+    >>> abs_value(-1)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def product(n, term):
+    """Return the product of the first n terms in a sequence.
+
+    n    -- a positive integer
+    term -- a function that takes one argument
+
+    >>> product(3, identity) # 1 * 2 * 3
+    6
+    >>> product(5, identity) # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)   # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)   # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    """
+    "*** YOUR CODE HERE ***"
+
+def factorial(n):
+    """Return n factorial for n >= 0 by calling product.
+
+    >>> factorial(4)
+    24
+    >>> factorial(6)
+    720
+    """
+    "*** YOUR CODE HERE ***"
+
+
+from operator import add, mul
+
+def accumulate(combiner, base, n, term):
+    """Return the result of combining the first n terms in a sequence.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)   # 2 * 1^2 * 2^2 * 3^2
+    72
+    """
+    "*** YOUR CODE HERE ***"
+
+def summation_using_accumulate(n, term):
+    """An implementation of summation using accumulate.
+
+    >>> summation_using_accumulate(5, square)
+    55
+    >>> summation_using_accumulate(5, triple)
+    45
+    """
+    "*** YOUR CODE HERE ***"
+
+def product_using_accumulate(n, term):
+    """An implementation of product using accumulate.
+
+    >>> product_using_accumulate(4, square)
+    576
+    >>> product_using_accumulate(6, triple)
+    524880
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def repeated(f, n):
+    """Return the function that computes the nth application of f.
+
+    >>> add_three = repeated(increment, 3)
+    >>> add_three(5)
+    8
+    >>> repeated(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    243
+    >>> repeated(square, 2)(5) # square(square(5))
+    625
+    >>> repeated(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    """
+    "*** YOUR CODE HERE ***"
+
+def compose1(f, g):
+    """Return a function h, such that h(x) = f(g(x))."""
+    def h(x):
+        return f(g(x))
+    return h
+
+###################
+# Church Numerals #
+###################
+
+def zero(f):
+    return lambda x: x
+
+def successor(n):
+    return lambda f: lambda x: f(n(f)(x))
+
+def one(f):
+    """Church numeral 1: same as successor(zero)"""
+    "*** YOUR CODE HERE ***"
+
+def two(f):
+    """Church numeral 2: same as successor(successor(zero))"""
+    "*** YOUR CODE HERE ***"
+
+three = successor(two)
+
+def church_to_int(n):
+    """Convert the Church numeral n to a Python integer.
+
+    >>> church_to_int(zero)
+    0
+    >>> church_to_int(one)
+    1
+    >>> church_to_int(two)
+    2
+    >>> church_to_int(three)
+    3
+    """
+    "*** YOUR CODE HERE ***"
+
+def add_church(m, n):
+    """Return the Church numeral for m + n, for Church numerals m and n.
+
+    >>> church_to_int(add_church(two, three))
+    5
+    """
+    "*** YOUR CODE HERE ***"
+
+def mul_church(m, n):
+    """Return the Church numeral for m * n, for Church numerals m and n.
+
+    >>> four = successor(three)
+    >>> church_to_int(mul_church(two, three))
+    6
+    >>> church_to_int(mul_church(three, four))
+    12
+    """
+    "*** YOUR CODE HERE ***"
+
+def pow_church(m, n):
+    """Return the Church numeral m ** n, for Church numerals m and n.
+
+    >>> church_to_int(pow_church(two, three))
+    8
+    >>> church_to_int(pow_church(three, two))
+    9
+    """
+    "*** YOUR CODE HERE ***"
+
