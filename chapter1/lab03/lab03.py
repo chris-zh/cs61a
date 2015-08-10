6c5905eb339a9d13fb2e40ac2bc3bcf127cdb316
chris.zh <chris.zh@outlook.com>
2015-08-10 17:51:29 +0800

    Up to Q12 of lab03.py

--- chapter1/lab03/lab03.py
+++ chapter1/lab03/lab03.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 # Q2
 def make_buzzer(n):
     """ Returns a function that prints numbers in a specified
@@ -77,7 +78,7 @@ def sum(n):
     return n + sum(n - 1)
 
 # Q7
-
+##边界条件缺失
 def sum_every_other_number(n):
     """Return the sum of every other natural number 
     up to n, inclusive.
@@ -89,10 +90,12 @@ def sum_every_other_number(n):
     """
     if n == 0:
         return 0
+    elif n == 1:
+        return 1
     else:
-        return n + sum_every_other_number(n - 1)
+        return n + sum_every_other_number(n - 2)
 
-
+##没有正确return结果
 def fibonacci(n):
     """Return the nth fibonacci number.
     
@@ -104,7 +107,7 @@ def fibonacci(n):
     elif n == 1:
         return 1
     else:
-        fibonacci(n - 1) + fibonacci(n - 2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 # Q8
@@ -124,4 +127,59 @@ def hailstone(n):
     7
     """
     "*** YOUR CODE HERE ***"
-
+    if n == 1:
+        return n
+    else:
+        print(n)
+        if n % 2 ==0:
+            return hailstone(n // 2)
+        else:
+            return hailstone(n * 3 + 1)
+#Q9
+def cycle(f1, f2, f3):
+    """ Returns a function that is itself a higher order function
+    >>> def add1(x):
+    ...     return x + 1
+    >>> def times2(x):
+    ...     return x * 2
+    >>> def add3(x):
+    ...     return x + 3
+    >>> my_cycle = cycle(add1, times2, add3)
+    >>> identity = my_cycle(0)
+    >>> identity(5)
+    5
+    >>> add_one_then_double = my_cycle(2)
+    >>> add_one_then_double(1)
+    4
+    >>> do_all_functions = my_cycle(3)
+    >>> do_all_functions(2)
+    9
+    >>> do_more_than_a_cycle = my_cycle(4)
+    >>> do_more_than_a_cycle(2)
+    10
+    >>> do_two_cycles = my_cycle(6)
+    >>> do_two_cycles(1)
+    19
+    """
+    "*** YOUR CODE HERE ***"
+    def g(n):
+        if n % 3 == 0:
+            return f3
+        elif n % 3 == 1:
+            return f1
+        elif n % 3 == 2:
+            return f2
+    def h(n):
+        if n == 0:
+            return lambda x: x
+        else:
+            return lambda x: g(n)(h(n - 1)(x))
+    return h
+#Q10
+def lambda_curry2(func):
+    return lambda x: lambda y: func(x, y)
+#Q11
+20
+#Q12
+def path(m, n):
+    return 0
